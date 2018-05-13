5fb1c62e3bd5c62c429f058c17c8b5623634d16c
Riley Usagi <riley.usagi@gmail.com>
2018-05-13 10:50:12 +0300

    Init

--- 2048/ViewController.swift
+++ 2048/ViewController.swift
@@ -1,25 +1,46 @@
-//
-//  ViewController.swift
-//  2048
-//
-//  Created by Riley Usagi on 11.05.2018.
-//  Copyright © 2018 Riley Usagi. All rights reserved.
-//
-
 import UIKit
+import GameplayKit
+
+/// Формат положения экрана
+enum Orientation {
+  case vertical
+  case horizon
+}
+
+/// Направление движения
+enum Direction : Int {
+  case forward = 1
+  case backward = -1
+}
+
+/// Положение плиток на доске
+struct Position : Equatable{
+  let x : Int
+  let y : Int
+  
+  /// find previouse position according to the direction and orientation
+  func previousPosition(direction: Direction, orientation: Orientation) -> Position {
+    switch orientation {
+    case .vertical:
+      return Position(x: x, y: y - direction.rawValue)
+    case .horizon:
+      return Position(x: x - direction.rawValue, y: y)
+    }
+  }
+}
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  
 }
 
